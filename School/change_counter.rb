10b3058c84f49366637d89667698f1e5804cf40e
Zachary Picone <zacharypicone@gmail.com>
2019-06-18 10:06:59 +1200

    Edited something I can't remember

--- School/change_counter.rb
+++ School/change_counter.rb
@@ -9,7 +9,7 @@ money = $stdin.gets.to_f
 change = money - price
 leftover_change = change
 
-puts "You'll get $#{change} change, made out of: "
+puts "You'll get $#{change.round(2)} change, made out of: "
 cash_array.each do |i|
   print "#{(leftover_change / i).to_i} $#{i}, "
   leftover_change = leftover_change - (i * (leftover_change / i).to_i)
